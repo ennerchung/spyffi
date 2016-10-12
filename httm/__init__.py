530e06b82844e052fa4a6d08dca2d73329068f04
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-10-12 12:11:00 -0400

    Temporarily putting conversion scripts in this directory while debugging

--- httm/__init__.py
+++ httm/__init__.py
@@ -1,23 +1,37 @@
 # See https://docs.python.org/2/library/collections.html#collections.namedtuple for details
 from collections import namedtuple
 
-Parameters = namedtuple(
-    'Parameters',
+CalibratedTransformParameters = namedtuple(
+    'CalibratedTransformParameters',
     [
         'number_of_slices',  # number of slices to use in transformation, either 1 or 4
         'video_scale',  # electrons/ADU
         'compression',
-        'baseline_adu',  # parameter only for calibrated->raw, derived from data for raw->calibrated
-        'drift_adu',  # parameter only for calibrated->raw
-        'start_of_line_ringing',  # array of length 534, parameter only for calibrated->raw
+        'baseline_adu',
+        'drift_adu',
+        'start_of_line_ringing',
         'undershoot',
         'smear_ratio',
         'clip_level_adu',
         'pattern_noise'  # 2D array, slice dimensions (2078 by 534)
     ])
+CalibratedTransformParameters.__new__.__defaults__ = (None,) * len(CalibratedTransformParameters._fields)
 
-FITSData = namedtuple(
-    'FITSData',
+RAWTransformParameters = namedtuple(
+    'RAWTransformParameters',
+    [
+        'number_of_slices',  # number of slices to use in transformation, either 1 or 4
+        'video_scale',  # electrons/ADU
+        'compression',
+        'undershoot',
+        'smear_ratio',
+        'clip_level_adu',
+        'pattern_noise'  # 2D array, slice dimensions (2078 by 534)
+    ])
+RAWTransformParameters.__new__.__defaults__ = (None,) * len(RAWTransformParameters._fields)
+
+FITSMetaData = namedtuple(
+    'FITSMetaData',
     ['origin_file_name',
      'header'])
 
@@ -30,67 +44,63 @@ Slice = namedtuple(
      'top_dark_pixels_rows',
      'left_dark_pixels_columns',
      'right_dark_pixels_columns',
-     'parameters',
      'pixel_data'])
+Slice.__new__.__defaults__ = (None,) * len(Slice._fields)
 
 RAWTransformation = namedtuple(
     'RAWTransformation',
-    ['number_of_slices'])
+    ['slices',
+     'fits_metadata',
+     'parameters'])
 
 CalibratedTransformation = namedtuple(
     'CalibratedTransformation',
-    ['number_of_slices'])
+    ['slices',
+     'fits_metadata',
+     'parameters'])
 
 
-def make_slice_from_calibrated_data(pixel_data, index, parameters):
+def make_slice_from_calibrated_data(pixel_data, index):
     """Construct a slice from an array of calibrated pixel data and a specified index"""
     return Slice(pixel_data=pixel_data,
                  index=index,
-                 parameters=parameters,
-                 unit='electrons')
+                 units='electrons')
 
 
-def calibrated_transform_from_hdulist(hdulist, number_of_slices=4, **kwargs):
-    """Construct a CalibratedTransformation from an astropy.io.fits.HDUList"""
-    from numpy import hsplit
-    assert len(hdulist) == 1, "Only a single image per FITS file is supported"
-    assert hdulist[0].data.shape[1] % number_of_slices == 0, \
-        "Image did not have the specified number of slices"
-    parameters = Parameters(number_of_slices=number_of_slices, **kwargs)
-    return CalibratedTransformation(
-        slices=map(lambda pixel_data, index:
-                   make_slice_from_calibrated_data(pixel_data, index, parameters),
-                   zip(hsplit(hdulist[0].data, number_of_slices), range(number_of_slices))))
-
-
-def calibrated_transform_from_file(file_name, **kwargs):
+def calibrated_transform_from_file(file_name, number_of_slices=4, **kwargs):
     """Construct a CalibratedTransformation from a file or file name"""
     from astropy.io import fits
-    return calibrated_transform_from_hdulist(fits.open(file_name), **kwargs)
+    from numpy import hsplit
+    header_data_unit_list = fits.open(file_name)
+    assert len(header_data_unit_list) == 1, "Only a single image per FITS file is supported"
+    assert header_data_unit_list[0].data.shape[1] % number_of_slices == 0, \
+        "Image did not have the specified number of slices"
+    return CalibratedTransformation(
+        slices=map(lambda (pixel_data, index):
+                   make_slice_from_calibrated_data(pixel_data, index),
+                   zip(hsplit(header_data_unit_list[0].data, number_of_slices), range(number_of_slices))),
+        fits_metadata=None,
+        parameters=CalibratedTransformParameters(number_of_slices=number_of_slices, **kwargs))
 
 
-def make_slice_from_raw_data(pixel_data, index, parameters):
+def make_slice_from_raw_data(pixel_data, index):
     """Construct a slice from an array of calibrated pixel data and a specified index"""
     return Slice(pixel_data=pixel_data,
                  index=index,
-                 parameters=parameters,
-                 unit='electrons')
+                 units='hdu')
 
 
-def raw_transform_from_hdulist(hdulist, number_of_slices=4, **kwargs):
-    """Construct a RAWTransformation from an astropy.io.fits.HDUList"""
-    from numpy import hsplit
-    assert len(hdulist) == 1, "Only a single image per FITS file is supported"
-    assert hdulist[0].data.shape[1] % number_of_slices == 0, \
-        "Image did not have the specified number of slices"
-    parameters = Parameters(number_of_slices=number_of_slices, **kwargs)
-    return RAWTransformation(
-        slices=map(lambda pixel_data, index:
-                   make_slice_from_raw_data(pixel_data, index, parameters),
-                   zip(hsplit(hdulist[0].data, number_of_slices), range(number_of_slices))))
-
-
-def raw_transform_from_file(file_name, **kwargs):
+def raw_transform_from_file(file_name, number_of_slices=4, **kwargs):
     """Construct a RAWTransformation from a file or file name"""
     from astropy.io import fits
-    return raw_transform_from_hdulist(fits.open(file_name), **kwargs)
+    from numpy import hsplit
+    header_data_unit_list = fits.open(file_name)
+    assert len(header_data_unit_list) == 1, "Only a single image per FITS file is supported"
+    assert header_data_unit_list[0].data.shape[1] % number_of_slices == 0, \
+        "Image did not have the specified number of slices"
+    return RAWTransformation(
+        slices=map(lambda (pixel_data, index):
+                   make_slice_from_raw_data(pixel_data, index),
+                   zip(hsplit(header_data_unit_list[0].data, number_of_slices), range(number_of_slices))),
+        fits_metadata=None,
+        parameters=RAWTransformParameters(number_of_slices=number_of_slices, **kwargs))
